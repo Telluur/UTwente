37efc75a164671adcfd221c0d87a9e946c7aa570
Michiel te Braake <m.tebraake.95@gmail.com>
2014-01-06 23:45:30 +0100

    This commit is not one to one

--- src/ss/week5/MapUtils.java
+++ src/ss/week5/MapUtils.java
@@ -1,9 +1,28 @@
 package ss.week5;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class MapUtils {
 	
-	public boolean isOneOnOne(){
+	public static void main(String[] args){
+		HashMap<String, Integer> testMap = new HashMap<String, Integer>();
+		testMap.put("test", 12);
+		testMap.put("test2", 3);
+		testMap.put("test3", 6);
+		testMap.put("test4", 9);
+
+		System.out.println(new MapUtils().isOneOnOne(testMap));
+	}
+	
+	public <K,V> boolean isOneOnOne(HashMap<K, V> map){
+		HashSet<V> tempSet = new HashSet<V>();
 		
+		for(V value : map.values()){
+			if(!tempSet.add(value)){
+				return false;
+			}
+		}
 		return true;
 	}
 }
