1c4af98567226e0823999c07a525618fd4eb8916
Michiel te Braake <m.tebraake.95@gmail.com>
2014-01-08 09:08:16 +0100

    composeren

--- src/ss/week5/MapUtils.java
+++ src/ss/week5/MapUtils.java
@@ -1,7 +1,6 @@
 package ss.week5;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class MapUtils {
 	
@@ -11,11 +10,16 @@ public class MapUtils {
 		testMap.put("test2", 3);
 		testMap.put("test3", 6);
 		testMap.put("test4", 9);
+        HashMap<Integer, Double> testMap2 = new HashMap<Integer, Double>();
+        testMap2.put(12, 1.0);
+        testMap2.put(3, 2.0);
+        testMap2.put(6, 3.0);
+        testMap2.put(9, 4.0);
 
-		System.out.println(new MapUtils().isOneOnOne(testMap));
+        System.out.println(new MapUtils().compose(testMap, testMap2));
 	}
 	
-	public <K,V> boolean isOneOnOne(HashMap<K, V> map){
+	public static <K,V> boolean isOneOnOne(HashMap<K, V> map){
 		HashSet<V> tempSet = new HashSet<V>();
 		
 		for(V value : map.values()){
@@ -25,4 +29,33 @@ public class MapUtils {
 		}
 		return true;
 	}
+
+    public static <K,V, K2, V2> boolean compatible(HashMap<K, V> map1, HashMap<K2, V2> map2){
+        for(V value : map1.values()){
+            if(!map2.containsKey(value)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <K,V, K2, V2> HashMap<K, V2> compose(HashMap<K, V> map1, HashMap<K2, V2> map2){
+        HashMap<K, V2> returnMap = new HashMap<>();
+        if(compatible(map1, map2)){
+            for(Map.Entry<K, V> entry : map1.entrySet()){
+                returnMap.put(entry.getKey(), map2.get(entry.getValue()));
+            }
+        }
+        return returnMap;
+    }
+
+    public static <K> List<K> merge(List<K> list){
+        LinkedList<K> sortedList = new LinkedList<>();
+        return sortedList;
+        //LinkedList no access to underlying nodes? What is the advantage of linkedlist over arraylist? (very slow sort implementation?)
+
+    }
+
+    private static <K> void getMiddle(LinkedList<K> list){
+    }
 }
