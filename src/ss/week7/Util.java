928b2234a74153b91543ca5d3a03f4610866494a
Michiel te Braake <m.tebraake.95@gmail.com>
2014-01-17 00:00:45 +0100

    Make Util follow checkstyle coding conventions

--- src/ss/week7/Util.java
+++ src/ss/week7/Util.java
@@ -1,31 +1,52 @@
 package ss.week7;
 
 import java.util.ArrayList;
-import java.lang.String;
 import java.util.List;
 
-public class Util {
-    
-    public static <Element> List<Element> zip
-    (List<Element> l1, List<Element> l2) {
-        ArrayList<Element> RESULT = new ArrayList<Element>();
-        for (int i = 0; i<l1.size(); i++) {
-            RESULT.add(l1.get(i));
-            RESULT.add(l2.get(i));
-        }
-        return RESULT;
-    }    
+/**
+ * Util methods.
+ * @author michiel
+ */
+public final class Util {
+
+	/**
+	 * Private constructor.
+	 */
+	private Util() {
+	}
 
     /**
-     * signum function
-     * @param i the function argument
-     * @return -1, 0 or 1 if the argument is negative, 0 or positive
-     */
-    public static int signum(int i) {
-        if(i < 0)
-            return -1;
-        else if (i > 0)
-            return 1;
-        else return 0;
-    }
+     * Zip two list.
+	 *
+	 * @param <Element> List object type
+	 * @param l1 List 1
+	 * @param l2 List 2
+	 * @return Zipped list
+	 */
+	public static <Element> List<Element>
+		zip(final List<Element> l1, final List<Element> l2) {
+		ArrayList<Element> result = new ArrayList<Element>();
+		for (int i = 0; i < l1.size(); i++) {
+			result.add(l1.get(i));
+			result.add(l2.get(i));
+		}
+		return result;
+	}
+
+	/**
+	 * signum function.
+	 *
+	 * @param i
+	 *            the function argument
+	 * @return -1, 0 or 1 if the argument is negative, 0 or positive
+	 */
+	public static int signum(final int i) {
+		if (i < 0) {
+			return -1;
+		} else if (i > 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }
